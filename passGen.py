ce7e9e3bc011cb24787293884f812519b203b3ec
Bavyeshwar <eshwar.thedardnian@gmail.com>
2026-04-26 10:07:25 -0500

    Implemented mnemonic password generation
    Refactored mnemonic password generation to use mnemonic phrases.

--- passGen.py
+++ passGen.py
@@ -1,44 +1,34 @@
 import string
 import secrets
+from xkcdpass import xkcd_password as xp
 
-EXCLUDE = ['\\', '\'', '"', ',', '.', '<', '>', '[', ']', '{', '}', '`', '~', ';', '/', ':', '|', '&']
-RARE_PUNCT = ['.', '>', '<', '~', '[', ']', ':', '|', '&']
+MNEMONIC_PHRASES = [
+   " spotted a ",
+   " jumped over the ",
+   " talked to a ",
+   " hid behind a "
+]
+MNEMONIC_CONJUNCTIONS = [
+   " while a ",
+   " then  a ",
+   " because the ",
+   " and suddenly a "
+]
+def generate_mnemonic(length):
+   connectors = ['-', '', '|', '_']
+   for i in range(4):
+      for j in range(1, 4):
+         connectors.append(f"{connectors[i] * j}")
+   connector = secrets.choice(connectors)
+    
+   pw = xp.generate_xkcdpassword(xp.generate_wordlist(xp.locate_wordfile(), min_length=5, max_length=8), numwords=length, delimiter=connector)
 
-def transform(phrase):
-    words = phrase.split()
-    letters = []
-    for word in words:
-        for letter in word: letters.append(letter)
-    exclude = ['"', '\'', '\\', '<', '>', '/', '?', ',', '.', '`', '~', ']', '[', '}', '{', ';']
-    pool = [char for char in string.punctuation if char not in exclude]
-    for char in string.digits: pool.append(char)
-    subs = {
-        'A': '@',
-        'E': '3',
-        'H': '|-|',
-        'I': '!',
-        'L': '|_',
-        'O': '0',
-        'S': '$',
-        'V': '\\/'
-    }
-
-    password = []
-    for i in range(secrets.randbelow(5)): password.append(secrets.choice(pool))
-
-    for letter in letters:
-        uppercase = letter.upper()
-        if uppercase in subs.keys():
-            password.append(subs[uppercase])
-        else:
-            password.append(secrets.choice([uppercase, uppercase.lower()]))
-
-    min = 15
-    while sum(1 for _ in password) < min:
-        extra = secrets.choice(pool)
-        if extra not in password: password.append(extra)
-
-    return "".join(password)
+   words = [word for word in pw.split(connector)]
+   mnemonic = words[0].capitalize()
+   for i in range(1, len(words)):
+      mnemonic += secrets.choice(MNEMONIC_CONJUNCTIONS) if i % 3 == 0 else secrets.choice(MNEMONIC_PHRASES)
+      mnemonic += words[i].lower()
+   return {"mnemonic": mnemonic, "pw": pw}
 def generate(punct, extra_punct, uppercase, lowercase, digits, length):
     if not isinstance(punct, bool) or not isinstance(extra_punct, bool) or not isinstance(uppercase, bool) or not isinstance(lowercase, bool) or not isinstance(digits, bool) or not isinstance(length, int): return -1
 
